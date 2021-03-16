5ba2303163cd7173b13c96a6a95086dd2f98d311
josephprado <pradodje@gmail.com>
2021-03-15 17:36:23 -0700

    update comments

--- WheelOfFortune.java
+++ WheelOfFortune.java
@@ -14,7 +14,7 @@ public class WheelOfFortune {
       Puzzle puzzle = new Puzzle();
       Game game = new Game();
       
-      // game rules, configure settings, and setup the wheel
+      // print game rules, configure settings, and setup the wheel
       settings.printGameRules();
       settings.gameSetup();
       listOfPlayers = settings.getListOfPlayers();
@@ -25,18 +25,22 @@ public class WheelOfFortune {
       // continues unitl player chooses not to play another game
       do {
          puzzle.generatePuzzle("ListOfPuzzles.csv");
+
          // game loop
          // continues until game is over (puzzle is solved or max number of rounds have expired)
          while (!game.getIsGameOver()) {
+
             // player loop
             // loops through each player until game is over
             for (currentPlayer = 0; currentPlayer < listOfPlayers.length; currentPlayer++) {
+
                // round loop
-               // each player continues their round until one of the following:
+               // each player's round continues until one of the following:
                // a) player makes a wrong guess, b) player goes bankrupt, or c) game is over
                do {
                   game.playRound(puzzle, listOfPlayers[currentPlayer], settings, wheel);
                } while (puzzle.getIsGuessCorrect() && !wheel.getIsBankrupt() && !game.getIsGameOver());
+
                // break player loop if game is over
                if (game.getIsGameOver()) {
                   break;
